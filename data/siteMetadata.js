c61727eda1635ab023e0f3643e6c10a8b8aa55af
Manolo Recio Sjögren <recio.sjogren@gmail.com>
2022-01-24 17:00:41 +0100

    new readme page and deleted images

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,7 +2,7 @@ const siteMetadata = {
   title: "Hi! I'm Manolo 👋",
   author: 'Manolo Recio Sjogren',
   headerTitle: 'Manolo Recio Sjogren',
-  description: "I'm a designer and technologist writing (and talking) about tech",
+  description: "I'm Manolo, ",
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
